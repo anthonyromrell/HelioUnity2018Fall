ba4f7f310cadf90d737b10d4db5b92e85015abe3
Anthony Romrell <anthony@artisandream.com>
2018-09-11 22:46:24 -0600

    Updated Unity Project

---
+++ HelioFallClass/Assets/EventScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+	public UnityEvent Event;
+	private bool OnButton;
+
+	private void OnMouseDown()
+	{
+		if (!OnButton) return;
+			Event.Invoke();
+	}
+}
