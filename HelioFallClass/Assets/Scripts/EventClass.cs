724af983168741463287eb875d71db876485cb01
Anthony Romrell <anthony@artisandream.com>
2018-09-13 20:24:11 -0600

    Added Events

---
+++ HelioFallClass/Assets/Scripts/EventClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventClass : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	void OnMouseDown()
+	{
+		Event.Invoke();
+	}
+
+	void OnTriggerEnter()
+	{
+		Event.Invoke();
+	}
+}
