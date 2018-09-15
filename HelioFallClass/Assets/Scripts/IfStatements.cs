5cad9d5ed4287fb5e282019d2b31e3993730e229
Anthony Romrell <anthony@artisandream.com>
2018-09-14 23:18:47 -0600

    Adding tools

---
+++ HelioFallClass/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	public int A;
+	public int B;
+	public int C;
+	
+	public string Password;
+	public bool OnSwitch;
+	
+	void Update () {
+		
+		if (OnSwitch)
+		{
+			print("The Light is on.");
+		}
+
+		if (Password == "OU812")
+		{
+			print("Correct Password.");
+		}
+
+		if (A + B == C)
+		{
+			print("A pluse B is equal to C.");
+		}		
+	}
+}
