e573406bbba0e0638a5d1f9232065477e4f00f6c
Anthony Romrell <anthonyromrell@mac.com>
2018-09-22 21:28:28 -0600

    Counter with While Loops

---
+++ HelioFallClass/Assets/Scripts/UI/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+	private Text counterLabel;
+	public float seconds = 1.0f;
+	public int numbers = 3;
+
+	IEnumerator Start ()
+	{
+		counterLabel = GetComponent<Text>();
+		
+		while (numbers > 0)
+		{
+			counterLabel.text = numbers.ToString();
+			yield return new WaitForSeconds(seconds);
+			numbers--;
+		}
+		
+		counterLabel.text = "Go!";
+		yield return new WaitForSeconds(2);
+		counterLabel.text = "";
+	}
+}
