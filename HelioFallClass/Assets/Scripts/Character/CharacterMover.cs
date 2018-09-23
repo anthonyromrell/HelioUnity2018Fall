c4fc24910d46533d5838c22feb21a2d7d0656b5d
Anthony Romrell <anthonyromrell@mac.com>
2018-09-22 20:50:34 -0600

    Added Jump

--- HelioFallClass/Assets/Scripts/Character/CharacterMover.cs
+++ HelioFallClass/Assets/Scripts/Character/CharacterMover.cs
@@ -1,38 +1,37 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 position;
+    private Vector3 rotation;
+    public float Speed = 3.0f;
+    private float JumpSpeed = 4.0f;
+    public float Gravity = 9.81f;
 
-	public CharacterController Controller;
-	private Vector3 position;
-	private Vector3 rotation;
-	public float Speed = 3.0f;
-	public float Gravity = 9.81f;
-	private float JumpSpeed = 4.0f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Controller.isGrounded)
-		{
-			position.Set(Speed*Input.GetAxis("Vertical"),0,0);
-			rotation.Set(0,Input.GetAxis("Horizontal"),0);
-			transform.Rotate(rotation);
-			position = transform.TransformDirection(position);
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            position.Set(Speed * Input.GetAxis("Vertical"), 0, 0);
+            rotation.Set(0, Input.GetAxis("Horizontal"), 0);
+            transform.Rotate(rotation);
+            position = transform.TransformDirection(position);
 
-			if (Input.GetButton("Jump"))
-			{
-				position.y = JumpSpeed;
-			}
-		}
+            if (Input.GetButton("Jump"))
+            {
+                position.y = JumpSpeed;
+            }
+        }
 
-		position.y -= Gravity*Time.deltaTime;
-		
-		Controller.Move(position*Time.deltaTime);
-	}
+        position.y -= Gravity * Time.deltaTime;
+        
+        controller.Move(position * Time.deltaTime);
+    }
 }
