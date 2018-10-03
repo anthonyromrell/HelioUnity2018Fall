df7bb8ef69d74dff031b023fd6a64c76eb9a8919
Anthony Romrell <anthonyromrell@mac.com>
2018-10-02 22:33:12 -0600

    Updates

--- HelioFallClass/Assets/RandomColor.cs
+++ HelioFallClass/Assets/RandomColor.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 [CreateAssetMenu]
 public class RandomColor : ScriptableObject
+ {
+ 
+ 	public List<ColorID> ColorIdList;
+ 
+ 	public ColorID RandonColor;
+ 	
+ 	public void OnEnable()
+ 	{
+ 		var value = Random.Range(0, ColorIdList.Count);
+ 		RandonColor.Value = ColorIdList[value].Value;
+ 	}
+ }
+
+[CreateAssetMenu]
+public class RandomObject : ScriptableObject
 {
-
-	public List<ColorID> ColorIdList;
-
-	public ColorID RandonColor;
+	public List<Object> ObjectList;
+	public Object FinalObject;
 	
 	public void OnEnable()
 	{
-		var value = Random.Range(0, ColorIdList.Count);
-		RandonColor.Value = ColorIdList[value].Value;
+		var value = Random.Range(0, ObjectList.Count);
+		FinalObject = ObjectList[value];
 	}
 }
--- HelioFallClass/Assets/Scripts/Animals/Mammal.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Mammal : Animal
-{
-	public Color FurColor = Color.green;
-}
